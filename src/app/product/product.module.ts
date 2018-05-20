76da01ffb347ecc4314dde7f189cb1845b527e7d
Bram Borggreve <borggreve@gmail.com>
2018-05-20 01:51:30 -0500

    Lession 8: presentational components

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductListComponent } from './containers/product-list/product-list.component';
 import { ProductDetailComponent } from './containers/product-detail/product-detail.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ProductRoutingModule
   ],
-  declarations: [ProductListComponent, ProductDetailComponent]
+  declarations: [ProductListComponent, ProductDetailComponent, ProductsComponent, ProductComponent]
 })
 export class ProductModule { }
