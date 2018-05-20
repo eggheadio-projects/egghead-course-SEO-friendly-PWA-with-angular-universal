b9a1a34d2ba589e948cbe86a1e414d187ed5c748
Bram Borggreve <borggreve@gmail.com>
2018-05-20 00:45:40 -0500

    Lesson 04: ui module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Store Inline Template</h1>
     <router-outlet></router-outlet>
   `,
-  styles: [`
-    h1 {
-      color: hotpink;
-    }
-  `]
 })
 export class AppComponent {}
